7740fa08c94b55cdb80f832fb66ae4e6d527a55b
github username <github email address>
2018-01-28 21:51:52 -0500

    styling

--- src/components/TemplateList.js
+++ src/components/TemplateList.js
@@ -12,13 +12,12 @@ const TemplateList = props => {
     />
   ));
   return (
-    <div>
-      <h3 className="column-templates">
-        <img className="imageArrow" src={Diamond} />
-      </h3>
+    <div className="centeredT">
       <div className="column-templates">
+        <img className="imageArrow" src={Diamond} style={{ width: "100%" }} />
         <div className="ui grid">
-          <div className=" ui two column grid moveUp">
+          <div className=" ui two column grid moveUp centered">
+            <br />
             <br />
             {templates}
           </div>
